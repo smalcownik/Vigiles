ca98c2f42cf4a647747cc54c020109ea78341f37
Marek Szkodziński <marekstwola@gmail.com>
2016-06-16 13:11:06 +0200

    poczatek pracy nad dodawaniem imgs'ow z zewnetrznego requesta

---
+++ js/ImageDataAdding.js
@@ -0,0 +1,24 @@
+/**
+ * Created by marek on 16.06.16.
+ */
+define([], function () {
+
+    var exported = {};
+
+
+    exported.xxxxx = function () { // f-kcja wywołana w JsonBuilder.updateJson
+
+
+
+
+
+    };
+
+
+    return exported;
+
+});
+
+//TODO: obmyślić i dokładnie zaplanować jak zbudować tę funkcjonalność - gdzie wprowadzić jakie czynnności
+// tutaj bedą funkcje, odpowiedzialne za dodawanie nowych images'ów do danych
+// (budowanie JSONA i dodawanie zdjęcia do folderu na podtstawie pliku i kilku danych o lokalizacji zdjęcia)
