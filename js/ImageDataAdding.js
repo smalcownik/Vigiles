e3840d19f241e494b2e7d1999b4689352624d81c
Marek Szkodziński <marekstwola@gmail.com>
2016-06-27 21:53:24 +0200

    gg

--- js/ImageDataAdding.js
+++ js/ImageDataAdding.js
@@ -32,13 +32,18 @@ define([], function () {
 
     a.) kliknięcie powoduje wyskoczenie prompta z  : danymi i opcją wgrania pliku (adres zdjęcia,
         ID rodzica, pozycja, wymiary, punkty)
+
                 
     b.) zatwierdzneie spowoduje:
+
         I. zapisanie przez JsonBuilder json'a z nowym Patchem
-        II. aktualizacja w przeglądarce
+            i. buduje Patch (patrz ii.)
+            ii. dodaje go w positionables //viewer.positionable.push(new Patch(image,parent,data,i))
+        II. aktualizacja w przeglądarce // updateMyPosition lub update all positionables (nie mialem czasu tego rozstrzygnąć)
         III. - zdjęcie jest użyte ale nie jest zapisywane w archiwum - do tego trzeba kliknać save
 
     c.) kliknięcie save spowoduje:
+
         I. wysłanie przez UpdateOnServer nowej paczki danych (także ze zdjęciem)
         II.zadbać (poprzez serwer node'a) żeby nowy plik img zapisał się automatycznie w archiwum
             plików
