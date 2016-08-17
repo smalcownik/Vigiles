8bbd485bfa45ce3879d996ff257dc53cb5155d99
Marek Szkodziński <marekstwola@gmail.com>
2016-08-17 15:16:53 +0200

    nic

--- node/post_and_get_server.js
+++ node/post_and_get_server.js
@@ -39,7 +39,7 @@ function contentType(ext) {
 
 console.log("plik startuje");
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
 
     var body = []; // gdy to body chciałem dać poza createServer - jako zmienną globalną, to w request.on("data") wyskakiwał błąd że body "has no push method"
 
@@ -47,69 +47,76 @@ http.createServer(function(request, response) {
     console.log("server running");
 
     var filepath = '.' + (request.url == '/' ? path_file : request.url);
-    var  fileext = path.extname(filepath);
-    console.log("przed :"+filepath); // sciezka do miejsca przechowywania pliku
-    console.log("przed :"+fileext);  // rozszerzenie/typ pliku
+    var fileext = path.extname(filepath);
+
+    console.log("przed :" + filepath); // sciezka do miejsca przechowywania pliku
+    console.log("przed :" + fileext);  // rozszerzenie/typ pliku
 
 
     if (request.method == "POST") {
 
-        if(fileext == ".jpg"){
+
+        if (fileext == ".jpg") {
 
             console.log("fileext to jotpeg");
         }
 
+        if (fileext == ".json") {
 
-    request.on('error', function (err) {
-        console.error(err);
-
-    }).on('data', function (chunk) {
-        body.push(chunk);
-
-    }).on('end', function () {
-
-
-        //TODO: tutuaj popracować, żeby rozróżniało że idzie json (dodanie punkta) lub img (idzie nowe zdjęcie) i w drugim przypadku niech:
-        // może po fileext ??
-
-        // przesyła i zapisuje plik img na serverze
-
-        if(fileext == ".jpg"){
-
-            console.log("fileext to jotpeg");
+            console.log("fileext to JSON");
         }
 
-        //TODO: moze tutaj rozróżnić na img lub JSON
-        body = Buffer.concat(body).toString(); // bez "toString()" wychodzi zakodowany Buffer, ale działa"
 
-        //console.log(body);
-        console.log("method 2: " + request.method);
+        request.on('error', function (err) {
+            console.error(err);
 
-        fs.writeFile('.' +path_file, body, function (err) {
-            if (err) {
-                return console.log(err);
+        }).on('data', function (chunk) {
+            body.push(chunk);
+
+        }).on('end', function () {
+
+
+            //TODO: tutuaj popracować, żeby rozróżniało że idzie json (dodanie punkta) lub img (idzie nowe zdjęcie) i w drugim przypadku niech:
+            // może po fileext ??
+
+            // przesyła i zapisuje plik img na serverze
+
+            if (fileext == ".jpg") {
+
+                console.log("fileext to jotpeg");
             }
 
-            console.log("The file was saved!");
+            //TODO: moze tutaj rozróżnić na img lub JSON
+            body = Buffer.concat(body).toString(); // bez "toString()" wychodzi zakodowany Buffer, ale działa"
+
+            //console.log(body);
+            console.log("method 2: " + request.method);
+
+            fs.writeFile('.' + path_file, body, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            });
+
+            response.on('error', function (err) {
+                console.error(err);
+            });
+
+            response.statusCode = 200;
+            // response.setHeader('Content-Type', 'application/json'); // bez tego działa - o dziwo
+            response.setHeader('Access-Control-Allow-Origin', '*'); // to musi być bo wyrzuca błąd
+            response.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // to też musi być bo wyrzuca błąd
+            //response.write(JSON.stringify(responseBody));//żeby response.write działało musi mieć argument - jakiś string bo wyrzuci błąd
+            //response.end(body);
+            response.end(); // response.end musi być bo inaczej nie wykona się request.on("end".....
+
         });
-
-        response.on('error', function (err) {
-            console.error(err);
-        });
-
-        response.statusCode = 200;
-        // response.setHeader('Content-Type', 'application/json'); // bez tego działa - o dziwo
-        response.setHeader('Access-Control-Allow-Origin', '*'); // to musi być bo wyrzuca błąd
-        response.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // to też musi być bo wyrzuca błąd
-        //response.write(JSON.stringify(responseBody));//żeby response.write działało musi mieć argument - jakiś string bo wyrzuci błąd
-        //response.end(body);
-        response.end(); // response.end musi być bo inaczej nie wykona się request.on("end".....
-
-    });
-}
+    }
 
 
-    else{
+    else {
 
         //var stat = fs.readFileSync(path_file);
         var stat = fs.readFileSync(filepath);
@@ -120,13 +127,13 @@ http.createServer(function(request, response) {
 
         response.writeHead(200, contentType(fileext)
 
-         //   {
-          //  'Content-Type': 'text/json'
-        // ponizsze headery wklejone kilka linijek wyzej jso "response.setHeader"
+            //   {
+            //  'Content-Type': 'text/json'
+            // ponizsze headery wklejone kilka linijek wyzej jso "response.setHeader"
             //'Access-Control-Allow-Origin': '*',
             //'Access-Control-Allow-Headers': 'Content-Type'
 
-        //}
+            //}
 
         );
         response.write(stat);
@@ -135,11 +142,10 @@ http.createServer(function(request, response) {
     }
 
 
-}).listen(port, function(){
+}).listen(port, function () {
     //Callback triggered when server is successfully listening. H$
     console.log("Server listening on: http://localhost:%s", port);
 });
-
 
 
 // działa w pełni: GET, POST, otwiera img
