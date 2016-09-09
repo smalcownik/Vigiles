c4e04e02baa3b6b1f55cec46d69c7ca02ee4fb4d
Marek Szkodziński <marekstwola@gmail.com>
2016-09-09 13:07:06 +0200

    09.09.2016

--- node/post_and_get_server.js
+++ node/post_and_get_server.js
@@ -121,8 +121,9 @@ http.createServer(function (request, response) {
                     console.log("wyglad nowej sciezki: "+ bodyObject[0]);
                     console.log("wyglad nowej sciezki po obcieciu: "+ bodyObject[0][0]);
 
+                    var currentPath = "."+bodyObject[0][0];
 
-                    mkdirp(bodyObject[0][0], function (err) {
+                    mkdirp(currentPath, function (err) {
 
                         // path exists unless there was an error
 
