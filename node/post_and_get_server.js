aca65f3da46a5832e37ef3fea3b075eb91392463
Marek Szkodziński <marekstwola@gmail.com>
2016-09-09 12:19:30 +0200

    09.09.2016

--- node/post_and_get_server.js
+++ node/post_and_get_server.js
@@ -7,11 +7,11 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var mkdirp = require('mkdirp');
+var mkdirp = require('mkdirp'); // do tego musialem zainstalowa nmp i "npm install mkdirp" z nmp
 var json_data_file_path = "/data/test_arch/data1.json";
 //var path_image_folder = "/data/test_arch/data1.json";
 var port = 80;
-var data_for_new_patches = [];
+var data_for_new_patches;
 
 function contentType(ext) {
     var ct;
@@ -45,15 +45,13 @@ http.createServer(function (request, response) {
 
     console.log("method 1: " + request.method);
     console.log("server running");
-    console.log("request.url: "+ request.url);
+    console.log("request.url: " + request.url);
 
     var filepath = '.' + (request.url == '/' ? json_data_file_path : request.url);
     var fileext = path.extname(filepath);
 
-    console.log("path przed :" + filepath+ '\n' +  "wartosc request.url: " + request.url); // sciezka do miejsca przechowywania pliku
+    console.log("path przed :" + filepath + '\n' + "wartosc request.url: " + request.url); // sciezka do miejsca przechowywania pliku
     console.log("ext przed :" + fileext);  // rozszerzenie/typ pliku
-    
-   
 
 
     if (request.method == "POST") {
@@ -72,31 +70,34 @@ http.createServer(function (request, response) {
 
             if (fileext == ".jpg") { // request to post - jpg
 
-                console.log("fileext to jotpeg, a jego url: "+ request.url);
-
-                // szukaj danych do jpg'a
-
+                console.log("fileext to jotpeg, a jego url: " + request.url);
+                if (data_for_new_patches = !null) {
 
 
 
+
+
+                }
+                data_for_new_patches = null;
 
             }
 
+
             if (fileext == ".json") { // request to post - json
+
+
                 console.log("fileext to JSON");
 
                 body = Buffer.concat(body).toString();
 
-                //console.log("body: " + body);
-
                 var bodyObject = JSON.parse(body);
 
                 //console.log(bodyObject);
-                
-                if (bodyObject.hasOwnProperty("meta")){
 
-                  console.log("to jest data1.json bo ma property 'meta' ");
 
+                if (bodyObject.hasOwnProperty("meta")) {
+
+                    console.log("to jest data1.json bo ma property 'meta' ");
 
 
                     fs.writeFile('.' + json_data_file_path, body, function (err) {
@@ -109,13 +110,19 @@ http.createServer(function (request, response) {
 
                 }
 
-                else if(typeof bodyObject[0] === "string"){
+                else if (typeof bodyObject[0] === "string") {
 
                     console.log("to sa dane do Patcha");
 
-                    // przygotować sciezke dla kolejnego requesta
 
-                    data_for_new_patches.push(bodyObject);
+                    data_for_new_patches = bodyObject; // bO:[path - docelowa, promptedData, nextOriginalParent],pD: [ścieżka_pierwotna_pliku, dane jsona, parent.id lub "newParent" ]
+
+                    console.log("wyglad nowej sciezki: "+bodyObject[0]);
+
+                    /*mkdirp('/tmp/some/path/foo', function (err) {
+                        // path exists unless there was an error
+
+                    });*/
 
                     //TODO: 09-09-2016 tutaj teraz robic
                     // a. jesli jest parent - zapisać plik
@@ -128,32 +135,26 @@ http.createServer(function (request, response) {
                 };
 
 
-
-                response.on('error', function (err) {
-                    console.error(err);
-                });
-                response.statusCode = 200;
-                // response.setHeader('Content-Type', 'application/json'); // bez tego działa - o dziwo
-                response.setHeader('Access-Control-Allow-Origin', '*'); // to musi być bo wyrzuca błąd
-                response.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // to też musi być bo wyrzuca błąd
-                //response.write(JSON.stringify(responseBody));//żeby response.write działało musi mieć argument - jakiś string bo wyrzuci błąd
-                //response.end(body);
-                response.end(); // response.end musi być bo inaczej nie wykona się request.on("end".....
-
-
-
-
             }
 
+            response.on('error', function (err) {
+                console.error(err);
+            });
+            response.statusCode = 200;
+            // response.setHeader('Content-Type', 'application/json'); // bez tego działa - o dziwo
+            response.setHeader('Access-Control-Allow-Origin', '*'); // to musi być bo wyrzuca błąd
+            response.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // to też musi być bo wyrzuca błąd
+            //response.write(JSON.stringify(responseBody));//żeby response.write działało musi mieć argument - jakiś string bo wyrzuci błąd
+            //response.end(body);
+            response.end(); // response.end musi być bo inaczej nie wykona się request.on("end".....
 
 
         });
     }
 
 
-    else { // if (request.method ==! "POST") // to dotyczy przesyłania plików zdjęć z serwera
+    else { // if (request.method === "GET") // to dotyczy przesyłania plików zdjęć z serwera
 
-        //var stat = fs.readFileSync(json_data_file_path);
         var stat = fs.readFileSync(filepath);
 
         response.setHeader('Access-Control-Allow-Origin', '*'); // to musi być bo wyrzuca błąd
@@ -172,13 +173,12 @@ http.createServer(function (request, response) {
 
         );
         response.write(stat);
-
         response.end();
     }
 
 
 }).listen(port, function () {
-    //Callback triggered when server is successfully listening. H$
+    //Callback triggered when server is successfully listening.
     console.log("Server listening on: http://localhost:%s", port);
 });
 
