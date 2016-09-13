bc425feb09c515a64484040c9ce8679cda5673c4
Marek Szkodziński <marekstwola@gmail.com>
2016-09-13 12:53:19 +0200

    13.09.2016

--- js/AddImageToServerREQUEST.js
+++ js/AddImageToServerREQUEST.js
@@ -12,17 +12,15 @@ define([], function () {
         console.log(imageUrl);
 
         var http_request = new XMLHttpRequest();
-        http_request.open("POST", "http://52.30.81.203",false); // tutaj zmienić url na odpalony serwer na amazonie (dziala z serwerem
-        // Vigiles/node/node_post.js)
-        //dziala !
-        //mozna dac dla testow url pliku z dysku, powinno tam sie zapisac ale nie testowalem
+        http_request.open("POST", "http://52.30.81.203");
+      
 
         http_request.setRequestHeader("Content-Type","image/jpeg");
 
         /*http_request.onload = function(){ console.log("as as asas a");};*/ // to też może byc ale bez tego działa
 
-        http_request.send("nowe zdjecie");
-       // http_request.send(imageUrl);
+        http_request.send(imageUrl);
+       
 
     };
 
