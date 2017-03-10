727bce1ece6310eb4c3769106bd225b832adfb9c
Marek Szkodziński <marekstwola@gmail.com>
2017-03-10 13:13:49 +0100

    10.03

--- js/MapDataProviderREQUEST.js
+++ js/MapDataProviderREQUEST.js
@@ -47,9 +47,9 @@ define(['./MapData'], function (MapData) {
 
         }
 
-debugger;
-            exported.loadData = function(url,cb){ // to cb jest z Viever.loadURL()
 //debugger;
+            exported.loadData = function(url,cb){ // to cb jest z Viever.loadURL()
+debugger;
                 //makeRequest(url+'/data1.json', // url pliku z dysku - dysk odpowiada plikiem
                 makeRequest(url, // w app.js jako url server amazona i server noda odpowie plikiem
                     function(response){ // response to (http_request.responseText)
